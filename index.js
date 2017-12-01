2d43cb0106856a5903fc509e44d5c347569e0cd1
Juan Zerbini Berro <juanze82@hotmail.com>
2017-12-01 13:58:52 +0100

    Fix Callback rectangle.parameter:function() For .area and .perimeter as arrow empty functions

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var rect = require('./rectangle')
-function solveRectangle(l,b) {
-  console.log("solve rect. Where l=" +l+ " and b= " +b);
+function solveRectangle(x,y) {
+  console.log("solve rect. Where l=" +x+ " and b= " +y);
 
   // Esta funcion sera tiene un llamado a un callback con 2s de delay
   // una vez que pasa l, b... el tercer parametro que es una funcion ()
   // demorará xx segundos definidos en el setTimeout
 
-  rect(l,b, (err, rectangle)=>{
+  rect(x,y, (err, rectangle)=>{
     // How to handle errror
     if(err){
       console.log("ERROR", err.message);
     }
     else {
       // No se necesita pasar parametros en rectangle.area porque ya los posee el callback en rect ()
-      console.log("The area of the rectangle of dim L=" +l + " and B= " +b + " is: " + rectangle.area());
-      console.log("The perimeter of the rectangle of dim L=" +l + " and B= " +b + " is: " + rectangle.perimeter() );
+      console.log("The area of the rectangle of dim L=" +x + " and B= " +y + " is: " + rectangle.area());
+      console.log("The perimeter of the rectangle of dim L=" +x + " and B= " +y + " is: " + rectangle.perimeter() );
     }
   });
   console.log("--------------------");
--- rectangle.js
+++ rectangle.js
@@ -3,7 +3,7 @@
 
 // Here the x & y parameters will be available for the two functions: perimeter and area
 module.exports = (x,y,callback) =>{
-  if (x <=0 || y <=0) {
+  if (x<=0 || y <=0) {
     setTimeout(()=>
       // O pasas un error
       // The Error Object, will have attached the message that can be retrieved by the solveRectangle()
@@ -20,8 +20,9 @@ module.exports = (x,y,callback) =>{
           // this x & y values are provided by solveRectangle()
           // and now area & parameter are send as an Object so
           // instead of using "area =" this will become parameters of the funcion "rectangle.area:"
-          area: (x,y)=>(x*y),
-          perimeter: (x,y)=>(2*(x+y))
+          // Also is important to see that the paramter is defined as a "empty" () function
+          area: ()=>(x*y),
+          perimeter: ()=>(2*(x+y))
           // then in error handling if the current function callback resolves success
           // the
         }),
